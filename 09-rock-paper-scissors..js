ddccad7979951a5d05c7714dd9b79dcaee6c3186
SHIBU-PAUL <paulshibu369@gmail.com>
2023-12-14 22:51:02 +0530

    first commit

---
+++ 09-rock-paper-scissors..js
@@ -0,0 +1,111 @@
+/*local storage out*/
+const score =  JSON.parse(localStorage.getItem('score'));
+
+updateScoreElement(); 
+
+   let computerMove ='';
+
+   function playGame(playerMove){
+
+        pickCompuerMove();    
+        
+          let result = '';
+
+          if(playerMove === 'scissors'){
+
+                  if(computerMove === 'rock'){
+                  result ='You lose';
+                }
+                else if(computerMove === 'paper'){
+                  result ='You win';
+                }
+                else if(computerMove === 'scisser'){
+                  result = 'Tie';
+                }
+          }
+
+          else if(playerMove === 'paper'){
+
+            if(computerMove === 'rock'){
+                  result ='You win';
+                }
+                else if(computerMove === 'paper'){
+                  result ='Tie';
+                }
+                else if(computerMove === 'scisser'){
+                  result = 'You lose';
+                }
+
+          }
+
+          else if(playerMove === 'rock'){
+
+            if(computerMove === 'rock'){
+                  result ='Tie';
+                }
+                else if(computerMove === 'paper'){
+                  result ='You lose';
+                }
+                else if(computerMove === 'scisser'){
+                  result = 'You win';
+                }
+          }
+
+          if(result === 'You win'){
+            score.wins++;
+          }
+          else if(result === 'You lose'){
+            score.losses++;
+          }
+          else if(result === 'Tie'){
+            score.ties++;
+          }
+
+         /*local storage only supports string*/
+          localStorage.setItem('score',JSON.stringify(score));
+
+     updateScoreElement();     
+     
+     
+     document.querySelector('.js-result').innerHTML = result ;
+
+     document.querySelector('.js-moves').innerHTML
+      =  `You
+       <img src="images/${playermove}-emoji.png" class="move-icon">
+       <img src="images/${computerMove}-emoji.png" class="move-icon">
+       Computer`;
+          
+        }
+
+
+  function updateScoreElement(){
+
+          document.querySelector('.js-score')
+            .innerHTML = `Wins=${score.wins},Losses=${score.losses},Ties=${score.ties}`;
+
+
+        }
+         
+   
+
+  function pickCompuerMove(){
+
+    const randomNumber = Math.random() ;
+
+  
+
+        if (randomNumber >= 0 && randomNumber< 1/3){
+          computerMove ='rock';
+        }
+        else if (randomNumber >= 1/3 && randomNumber < 2/3){
+          computerMove ='paper';
+        }
+        else if (randomNumber >= 2/3 && randomNumber < 1){
+          computerMove ='scisser';
+        }
+
+        console.log(computerMove);
+
+       
+   
+  }
---
+++ 11-todo-list.js
@@ -0,0 +1,58 @@
+ const todoList = [{
+  name:'make dinner', 
+  dueDate:'2020-12-22'
+}, 
+{
+  name:'wash dishes',
+  dueDate:'2022-12-22'
+}];
+
+ randerTodoList();
+
+ function randerTodoList(){
+
+        let todoListHTML = '';
+
+    todoList.forEach(function(todoObject, index){
+          const todoObject = todoList[index];
+          const name = todoObject.name;
+          const dueDate = todoObject.dueDate;
+
+          /*generatinh HTML in the JS*/ 
+          const html = `
+           <div>${name}</div>
+           <div>${dueDate}</div> 
+           <button onclick ="
+            todoList.splice(${index}, 1);
+            randerTodoList();
+          " class="delete-todo-button">Delete</button>
+          `;
+          todoListHTML += html;
+        });
+
+
+      document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+ }
+
+ 
+
+ function addTodo(){
+  const inputElement =document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dataInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dataInputElement.value;
+ 
+
+  todoList.push({
+    name: name,
+    dueDate: dueDate
+  });
+  
+
+
+  inputElement.value = '';
+
+  randerTodoList();
+ }
